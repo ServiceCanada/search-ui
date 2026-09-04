9947c9093bb55983ef613bc61a5dd2d676bff061
igor kisselev <91329098+igkislev@users.noreply.github.com>
2026-09-04 14:37:45 -0400

    Use DOMPurify to strip HTML tags
    Replaced HTML stripping method with DOMPurify for security.

--- src/suggestions.js
+++ src/suggestions.js
@@ -169,9 +169,7 @@ function buildCleanQueryString( paramsObject ) {
 
 // Strip HTML tags of a given string
  function stripHtml(html) {
-	let tmp = document.createElement( "DIV" );
-	tmp.innerHTML = html;
-	return tmp.textContent || tmp.innerText || "";
+	return DOMPurify.sanitize( html, { ALLOWED_TAGS: [], ALLOWED_ATTR: [] } );
 }
 // Initiate engine
 function initEngine() {
